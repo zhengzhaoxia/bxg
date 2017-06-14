135ef349df9d77cda849417f3a8737ac348903f6
zheng <848977416@qq.com>
2017-06-15 01:50:07 +0800

    完成添加和编辑讲师功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,19 +1,25 @@
 define(['jquery','util','template'],function($,util,template) {
 	util.setMenu('/teacher/list');
-	$('#addTeacherId').click(function() {
-		$.ajax({
-			type : 'post',
-			url : '/api/teacher/add',
-			data : $('#addTeacherForm').serialize(),
-			dataType : 'json',
-			success : function(data) {
-				// console.log(data);
-
-			}
-		});
-	});
 	//获取参数中的tc_id
 	var tcId = util.qs('tc_id',location.search);
+	//提交表单处理
+	function submitForm(url) {
+		$('#addTeacherId').click(function() {
+			$.ajax({
+				type : 'post',
+				url : url,
+				data : $('#addTeacherForm').serialize(),
+				dataType : 'json',
+				success : function(data) {
+					// console.log(data);
+					if(data.code == 200) {
+						location.href = '/teacher/list';
+					}
+
+				}
+			});
+		});
+	}
 	if(tcId){
 		//根据id查询数据
 		$.ajax({
@@ -27,12 +33,14 @@ define(['jquery','util','template'],function($,util,template) {
 				data.result.operateFlag = '编辑';
 				var html = template('addTeacherTpl',data.result);
 				$('#addTeacherInfo').html(html);
+				submitForm('/api/teacher/update');
 			}
 		});
 	}else {
 		$('#navsFlag').text('讲师添加');
-		var html = template('addTeacherTpl',{operateFlag : '添加'});
+		var html = template('addTeacherTpl',{operateFlag : '添加',tc_gender : 1});
 		$('#addTeacherInfo').html(html);
+		submitForm('/api/teacher/add');
 	}
 	
 });
