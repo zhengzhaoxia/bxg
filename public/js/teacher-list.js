617434fbd47e9dc6909c69ae6f0124768f216156
zheng <848977416@qq.com>
2017-06-14 16:16:19 +0800

    实现讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,13 +1,30 @@
-define(['jquery','template'],function($,template) {
+define(['jquery','template','bootstrap'],function($,template) {
 	//加载列表数据
 	$.ajax({
 		type : 'get',
 		url : '/api/teacher',
 		dataType : 'json',
 		success : function(data) {
-			console.log(data);
+			// console.log(data);
 			var html = template('teacherInfoTpl',{list:data.result});
 			$('#teacherInfo').html(html);
+			$('#teacherInfo').find('.preview').click(function() {
+				var tcId = $(this).closest('td').attr('data-id');
+				// console.log(tcId);
+				$.ajax({
+					type : 'get',
+					url : '/api/teacher/view',
+					data : {tc_id:tcId},
+					dataType : 'json',
+					success : function(data) {
+						var html = template('teacherModalTpl',data.result);
+						$('#teacherModalInfo').html(html);
+						$('#teacherModal').modal();
+					}
+				});
+				// 防止跳转的默认事件
+				return false;
+			});
 		}
 	});
 });
