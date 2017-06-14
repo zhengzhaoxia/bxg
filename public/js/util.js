cf33b4e033862b2601f726abc062981fbee3d8d8
zheng <848977416@qq.com>
2017-06-14 21:59:24 +0800

    实现根据id查询值的功能

--- public/js/util.js
+++ public/js/util.js
@@ -3,6 +3,20 @@ define(['jquery'],function($) {
 		setMenu : function(pathname) {
 			$('.aside .navs a').removeClass('active');
 			$('.aside .navs a[href="'+pathname+'"]').addClass('active');
-		}
+		},
+		qs : function(attr,param) {
+			var p = param.substring(1);
+			var arr = p.split('&');
+			var retValue = '';
+			arr.forEach(function(element,index) {
+				var kv = element.split('=');
+				if(attr == kv[0]) {
+					retValue = kv[1];
+					//结束循环
+					return;
+				}
+			});
+			return retValue;
+		}	
 	}
 });
