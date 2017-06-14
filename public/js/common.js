0cd269da1edace744e0609ecba6be074013915d4
zheng <848977416@qq.com>
2017-06-14 11:15:00 +0800

    引入模板引擎并且重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($) {
+define(['jquery','template','cookie'],function($,template) {
 	//控制左侧菜单的折叠和展开
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
@@ -25,8 +25,12 @@ define(['jquery','cookie'],function($) {
 	var loginInfo = $.cookie('loginInfo') && JSON.parse($.cookie('loginInfo'));
 	//如果存在，渲染页面
 	if(loginInfo) {
-		$('.aside .profile').find('img').attr('src',loginInfo.tc_avatar);
-		$('.aside .profile').find('h4').text(loginInfo.tc_name);
+		var loginTpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+		var html = template.render(loginTpl,loginInfo);
+		$('#loginInfoTpl').html(html);
+
+		// $('.aside .profile').find('img').attr('src',loginInfo.tc_avatar);
+		// $('.aside .profile').find('h4').text(loginInfo.tc_name);
 	}
 	// var pathname = location.pathname;
 	// console.log(pathname);
